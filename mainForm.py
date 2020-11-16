3e27829644b733147b94e95854f0cbe84759ed91
xzf199358 <1217158614@qq.com>
2020-11-16 20:27:50 +0800

    add

--- mainForm.py
+++ mainForm.py
@@ -8,6 +8,8 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+#we have a test for github
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
